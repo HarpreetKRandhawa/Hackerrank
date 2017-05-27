403a1ca1cfef16cf996ffc53ad53f2492e287e24
Code Begineer <hrandhaw@usc.edu>
2017-05-26 23:53:14 -0700

    Data Types

---
+++ 30-days-of-code/day1.java
@@ -0,0 +1,19 @@
+        /* Declare second integer, double, and String variables. */
+        int num_int = scan.nextInt();
+        double num_double = scan.nextDouble();
+        scan.nextLine();
+        String val = scan.nextLine();
+
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(num_int + i);
+        System.out.println(num_double + d);
+        System.out.println(s+val);
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+
