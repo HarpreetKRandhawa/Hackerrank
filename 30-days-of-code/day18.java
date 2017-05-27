aa70715d4fde068be2868c15c44100a28618acbb
Code Begineer <hrandhaw@usc.edu>
2017-05-26 23:34:13 -0700

    Queues and Stacks

---
+++ 30-days-of-code/day18.java
@@ -0,0 +1,15 @@
+public class Solution {
+    Stack<Character> stack = new Stack<Character>();
+    Queue<Character> queue = new LinkedList<Character>();
+    void pushCharacter(char ch){
+        stack.push(ch);
+    }
+    void enqueueCharacter(char ch){
+        queue.add(ch);
+    }
+    char popCharacter(){
+        return stack.pop();
+    }
+    char dequeueCharacter(){
+        return queue.remove();
+    }
