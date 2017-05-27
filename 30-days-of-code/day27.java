d6c5e85bb761be6c0edbba71b6cb8a410ae69cb5
Code Begineer <hrandhaw@usc.edu>
2017-05-26 23:26:42 -0700

    Testing

---
+++ 30-days-of-code/day27.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("5");
+        
+        System.out.println("3 2");
+        System.out.println("-1 0 1");
+        
+        System.out.println("8 4");
+        System.out.println("-1 -2 0 3 3 9 6");
+        
+        System.out.println("6 2");
+        System.out.println("-1 -2 -3 0 1 2");
+        
+        System.out.println("7 3");
+        System.out.println("-3 -2 -1 0 1 2 3");
+        
+        System.out.println("4 4");
+        System.out.println("-1 0 1 2");
+        
+    }
+}
