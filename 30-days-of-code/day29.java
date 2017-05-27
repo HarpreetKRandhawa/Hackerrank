0c12411062e136c97deddf16671234c8526f9693
Code Begineer <harpreet@Harpreets-MacBook-Air.local>
2017-05-26 23:23:10 -0700

    30 days of code
    It has my solutions to the 30 days of coding at Hackerrank.

---
+++ 30-days-of-code/day29.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0; i<t; i++){
+            int num = sc.nextInt();
+            int limit = sc.nextInt();
+            int max = -1;
+            for(int j=1; j<=num; j++){
+                for(int k=j+1; k<=num; k++){
+                    if((j&k) > max && (j&k) < limit){
+                        max = j&k;
+                    }
+                }
+            }
+            System.out.println(max);
+        }
+    }
+}
