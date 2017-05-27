addace4f2e98f5148c27b94de02137488fb4192a
Code Begineer <hrandhaw@usc.edu>
2017-05-26 23:41:41 -0700

    Inheritance

---
+++ 30-days-of-code/day12.java
@@ -0,0 +1,42 @@
+class Student extends Person{
+	private int[] testScores;
+
+    /*	
+    *   Class Constructor
+    *   
+    *   @param firstName - A string denoting the Person's first name.
+    *   @param lastName - A string denoting the Person's last name.
+    *   @param id - An integer denoting the Person's ID number.
+    *   @param scores - An array of integers denoting the Person's test scores.
+    */
+    // Write your constructor here
+    Student(String firstName, String lastName, int id, int[] testScores){
+        super(firstName, lastName, id);
+        this.testScores = testScores;
+    }
+
+    /*	
+    *   Method Name: calculate
+    *   @return A character denoting the grade.
+    */
+    // Write your method here
+    public char calculate(){
+        int sum = 0;
+        for(int i=0;i<testScores.length;i++){
+            sum += testScores[i];
+        }
+        double average = (float)sum/testScores.length;
+        if(average >= 90 && average <= 100)
+            return 'O';
+        else if(average >= 80 && average < 90)
+            return 'E';
+        else if(average >= 70 && average < 80)
+            return 'A';
+        else if(average >= 55 && average < 70)
+            return 'P';
+        else if(average >= 40 && average < 55)
+            return 'D';
+        else
+            return 'T';
+    }
+}
