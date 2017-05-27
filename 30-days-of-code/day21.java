8919ae0b3ef91c0e1de4dc3d6a4e9c218b5020a2
Code Begineer <hrandhaw@usc.edu>
2017-05-26 23:32:03 -0700

    Generics

---
+++ 30-days-of-code/day21.java
@@ -0,0 +1,12 @@
+    /**
+    *    Method Name: printArray
+    *    Print each element of the generic array on a new line. Do not return anything.
+    *    @param A generic array
+    **/
+    
+    // Write your code here
+    public <T> void printArray(T[] array){
+        for(T o:array){
+            System.out.println(o);
+        }
+    }
