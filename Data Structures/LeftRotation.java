9df842d31d4a414433fe0de8e9aa72267227426f
Code Begineer <hrandhaw@usc.edu>
2017-05-30 12:36:22 -0700

    Arrays Left Rotation

---
+++ Data Structures/LeftRotation.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int d = sc.nextInt();
+        int[] a = new int[n];
+        for(int i=0; i<n; i++)
+            a[i] = sc.nextInt();
+        while(d > 0){
+            int temp = a[0];
+            for(int i=0; i<n-1 ; i++){
+                a[i] = a[i+1];
+            }
+            a[n-1] = temp;
+            d--;
+        }
+        for(int i=0; i<n; i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
