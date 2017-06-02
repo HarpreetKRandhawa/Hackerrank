b26106e663f7ad2f6c0c1faefc596eef0855bc0e
Code Begineer <hrandhaw@usc.edu>
2017-06-01 22:35:28 -0700

    Delete Duplicates Values

---
+++ Data Structures/DeleteDuplicates.java
@@ -0,0 +1,22 @@
+/*
+Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+
+Node RemoveDuplicates(Node head) {
+    if(head == null){
+        return head;
+    }
+    Node temp = head;
+    while(temp.next != null){
+        if(temp.data == temp.next.data)
+            temp.next = temp.next.next;
+        else
+            temp = temp.next;
+    }
+    return head;
+}
+
