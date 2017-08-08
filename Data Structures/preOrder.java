39dc6be2e8b7207d9beac7be6ec856e98a6161ef
Code Begineer <hrandhaw@usc.edu>
2017-08-08 11:04:58 -0700

    Tree: PreOrder Traversal
    Complete the preOrder function in your editor below, which has  parameter: a pointer to the root of a binary tree. It must print the values in the tree's preorder traversal as a single line of space-separated values.
    
    Input Format
    
    Our hidden tester code passes the root node of a binary tree to your preOrder function.
    
    Constraints
    
     Nodes in the tree  
    
    Output Format
    
    Print the tree's preorder traversal as a single line of space-separated values.

---
+++ Data Structures/preOrder.java
@@ -0,0 +1,24 @@
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+*/
+
+void preOrder(Node root) {
+    Stack<Node> stack = new Stack<Node>();
+    stack.push(root);
+    while(!stack.isEmpty()){
+        Node curr = stack.pop();
+        if(curr.right != null)
+            stack.push(curr.right);
+        if(curr.left != null)
+            stack.push(curr.left);
+        System.out.print(curr.data+" ");
+    }
+}
+
